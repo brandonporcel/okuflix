f7d9a7c743c0e8f43825f6a202e07feb80d3e3bb
Familia Porcel <brandon7.7porcel@gmail.com>
2020-12-11 11:19:48 -0300

    me di cuenta que no puedo meter 2 for,voy a crear una funcion

--- js/app.js
+++ js/app.js
@@ -40,13 +40,15 @@ d.addEventListener('DOMContentLoaded', () => {
 const d = document;
 const $slider = d.querySelectorAll('.slider');
 const $rankSlider = d.querySelector('.rank-slider');
+const $animeSlider = d.querySelector('.anime-slider');
 const getData = async () => {
 	const baseUrl = 'http://www.omdbapi.com';
 	const key = 'c075c45e';
 	let i = 0;
 	let bsAsData;
 	let animeData;
-	let $template = '';
+	let $bsAsTemplate = '';
+	let $animeTemplate = '';
 	Promise.all([
 		fetch(`${baseUrl}?s=buenos+aires&apikey=${key}`),
 		fetch(`${baseUrl}?s=anime&apikey=${key}`),
@@ -57,14 +59,26 @@ const getData = async () => {
 		.then((json) => {
 			bsAsData = json[0].Search;
 			animeData = json[1].Search;
-			for (i; i < bsAsData.length; i += 1) {
-				$template += `<figure class="slider-figure">
-					<img class="slider-img" src="${bsAsData[i].Poster}" alt="${bsAsData[i].Title}" data-imdb-id="${bsAsData[i].imdbID}">
-					<figcaption>
-				</figcaption></figure>`;
+			// for (i; i < bsAsData.length; i += 1) {
+			// 	$bsAsTemplate += `<figure class="slider-figure">
+			// 		<img class="slider-img" src="${bsAsData[i].Poster}" alt="${bsAsData[i].Title}" data-imdb-id="${bsAsData[i].imdbID}">
+			// 		<figcaption>
+			// 	</figcaption></figure>`;
+			// }
+			// for (i; i < animeData.length; i += 1) {
+			// 	$animeTemplate += `<figure class="slider-figure">
+			// 		<img class="slider-img" src="${animeData[i].Poster}" alt="${animeData[i].Title}" data-imdb-id="${animeData[i].imdbID}">
+			// 		<figcaption>
+			// 	</figcaption></figure>`;
+			// 	console.log($animeTemplate);
+			// 	console.log(i);
+			// }
+			for (i; i < 5; i += 1) {
+				console.log(i);
 			}
-
-			$rankSlider.innerHTML = $template;
+			// console.log(animeData);
+			// $animeSlider.innerHTML = $animeTemplate;
+			// $rankSlider.innerHTML = $bsAsTemplate;
 		})
 		.catch((err) => console.log(err));
 };
