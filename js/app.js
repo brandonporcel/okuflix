347dba4fa92c28b96fce6496c2d8cefa6dc31d96
Familia Porcel <brandon7.7porcel@gmail.com>
2020-12-15 19:49:51 -0300

    estoy laburando en el nav,me esta costando

--- js/app.js
+++ js/app.js
@@ -1,9 +1,11 @@
 import { consumiendoApi } from './modules/movies_api.js';
 import { deslizador } from './modules/slider.js';
 import { cargaPaja } from './modules/video_lazy_load.js';
+import { contenidoPrincipal } from './modules/main_content.js';
 
 document.addEventListener('DOMContentLoaded', () => {
 	cargaPaja();
 	consumiendoApi();
 	deslizador();
+	contenidoPrincipal();
 });
---
+++ js/modules/main_content.js
@@ -0,0 +1,41 @@
+const d = document;
+export function contenidoPrincipal() {
+	const $movieInfo = d.getElementById('movie-info');
+	const $movieTitle = d.getElementById('movie-title');
+	const $navItems = d.querySelectorAll('.main-nav-item');
+	const api = async () => {
+		try {
+			const res = await fetch(
+				'http://www.omdbapi.com/?t=bojack+horseman+&apikey=c075c45e'
+			);
+			if (!res.ok) {
+				throw new Error({
+					status: res.status,
+					statusText: res.statusText,
+				});
+			}
+			const bojackInfo = await res.json();
+			$movieInfo.innerHTML = bojackInfo.Plot;
+			$movieTitle.innerHTML = bojackInfo.Title;
+		} catch (err) {
+			console.log(err);
+			const message = err.statusText || 'ocurrio un fallo con la informacion';
+			$movieTitle.classList.add('error');
+			$movieTitle.innerHTML = `error ${err.status}: ${message}`;
+		}
+	};
+	api();
+	const nav = () => {
+		d.querySelector('.nav-first-item').classList.add('active');
+		$navItems.forEach((item) => {
+			item.addEventListener('click', (e) => {
+				if (!e.target.classList.contains('active')) {
+					d.querySelector('.nav-first-item').classList.remove('active');
+					e.target.classList.add('active');
+				} else {
+				}
+			});
+		});
+	};
+	nav();
+}
