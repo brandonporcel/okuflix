8693714a8b6a8b43682857e7a154b976e36b3c17
Familia Porcel <brandon7.7porcel@gmail.com>
2020-12-18 13:40:41 -0300

    estoy

--- js/modules/searcher.js
+++ js/modules/searcher.js
@@ -2,13 +2,14 @@ const d = document;
 const w = window;
 export function buscador() {
 	const $searchInput = d.getElementById('search-input');
-	const $queryResultCtn = d.getElementById('query-result-ctn');
 	d.addEventListener('keyup', async (e) => {
 		if (e.target === $searchInput) {
 			if (e.key === 'Enter') {
 				try {
 					const query = $searchInput.value.toLowerCase();
 					const key = 'c075c45e';
+					const $templateContent = d.getElementById('query-template').content;
+					const $fragment = d.createDocumentFragment();
 					// $queryResultCtn.insertAdjacentHTML(
 					// 	'afterbegin',
 					// 	'<img class="loader" src="assets/img/loader.svg" alt="loader">'
@@ -16,6 +17,8 @@ export function buscador() {
 					// $queryResultCtn.innerHTML =
 					// 	'<img class="loader" src="assets/img/loader.svg" alt="loader">';
 					d.querySelector('.query-ctn').classList.remove('none');
+					d.querySelector('.query-ctn').innerHTML =
+						'<img class="loader" src="assets/img/loader.svg" alt="loader">';
 					w.scrollTo({
 						behavior: 'smooth',
 						top: 500,
@@ -24,6 +27,7 @@ export function buscador() {
 					const res = await fetch(
 						`http://www.omdbapi.com?t=${query}&apikey=${key}`
 					);
+
 					const movie = await res.json();
 					if (!res.ok) {
 						throw new Error({
@@ -31,21 +35,42 @@ export function buscador() {
 							statusText: res.statusText,
 						});
 					}
-					console.log(res);
-					console.log(movie);
-					// $queryResultCtn.innerHTML = '';
-					// $queryResultCtn.insertAdjacentHTML('afterbegin', '');
-					d.querySelector('.query-title').innerHTML = movie.Title;
-					d.querySelector('.query-poster').src = movie.Poster;
-					d.querySelector('.query-poster').alt = movie.Title;
-					d.querySelector('.query-plot').innerHTML = movie.Plot;
-					d.getElementById('query-actors').innerHTML = movie.Actors;
-					d.getElementById('query-writer').innerHTML = movie.Writer;
-					d.getElementById('query-genre').innerHTML = movie.Genre;
+					if (movie.Response === 'False') {
+						d.getElementById('query-result-ctn').innerHTML =
+							'no encontramos esa pelicula';
+					}
+					$templateContent.getElementById(
+						'query-title-result'
+					).innerHTML = `'${query}'`;
+					$templateContent.querySelector('.query-title').innerHTML =
+						movie.Title;
+					$templateContent.querySelector('.query-poster').alt = movie.Title;
+					$templateContent.querySelector('.query-poster').src = movie.Poster;
+					$templateContent.getElementById('query-year').innerHTML = movie.Year;
+					$templateContent.getElementById('query-rated').innerHTML =
+						movie.Rated;
+
+					$templateContent.getElementById('query-country').innerHTML =
+						movie.Country;
+					$templateContent.querySelector('.query-plot').innerHTML = movie.Plot;
+					$templateContent.getElementById('query-actors').innerHTML =
+						movie.Actors;
+					$templateContent.getElementById('query-writer').innerHTML =
+						movie.Writer;
+					$templateContent.getElementById('query-genre').innerHTML =
+						movie.Genre;
+
+					const $clone = d.importNode($templateContent, true);
+					$fragment.appendChild($clone);
+					d.querySelector('.query-ctn').innerHTML = '';
+					d.querySelector('.query-ctn').appendChild($fragment);
 				} catch (err) {
 					const message = err.statusText || 'ocurrio un error';
-					$queryResultCtn.classList.add('error');
-					$queryResultCtn.innerHTML = `error ${err.status}: ${message}`;
+					console.log(err);
+					// d.getElementById('query-result-ctn').classList.add('error');
+					// d.getElementById(
+					// 	'query-result-ctn'
+					// ).innerHTML = `error ${err.status}: ${message}`;
 				}
 			}
 		}
